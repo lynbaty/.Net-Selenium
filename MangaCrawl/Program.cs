1f5c76cc8ed1f6a187b26d41d8e485cacd21c3f7
Ku Ân <boanku@gmail.com>
2023-11-20 21:00:45 +0700

    add git ignore

---
+++ MangaCrawl/Program.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MangaCrawl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://truyenqqvn.com/truyen-tranh/one-piece-128");
+
+            var elements = driver.FindElements(By.XPath("//*[contains(@class, 'works-chapter-item')]/div/a"));
+            var links = elements.ToArray().Reverse().ToList();
+            var mangaName = "OnePiece";
+            var chapterCount = 1;
+
+            foreach ( var link in links )
+            {
+                var url = link.GetAttribute("href");
+                driver.Navigate().GoToUrl(url);
+                var imgElements = driver.FindElements(By.XPath("//*[contains(@class, 'page-chapter')]/img"));
+                var imgSources = imgElements.ToArray().Select(i => i.GetAttribute("src"));
+                var pageCount = 1;
+                foreach (var img in imgSources)
+                {
+                    var fileName = "D:\\\\MangaCrawl\\" + mangaName + "_" + chapterCount.ToString("00") + "_" + pageCount.ToString("00") + ".jpg";
+                    using (var client = new HttpClient())
+                    {
+                        using (var s = client.GetStreamAsync(img))
+                        {
+                            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
+                            {
+                                s.Result.CopyTo(fs);
+                            }
+                        }
+                    }
+                    pageCount++;
+                }
+                chapterCount++;
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
